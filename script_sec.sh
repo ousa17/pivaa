97b8e946ca792636d2db6e55f3bbe2193f37103f
Oussama BEN WAFI <oussamabenwafi@MAC371.local>
2019-07-11 10:26:25 +0200

    script sec

--- script_sec.sh
+++ script_sec.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 pwd
 printf "\n1 - upload **********\n"
-authorisation = "3fc39aaa54925c9b4a311322d731023295e0e1618f0668695049b07924ca81ef"
-upload_output=`curl -F  'file=@app/build/outputs/apk/debug/app-debug.apk' http://localhost:8000/api/v1/upload -H "Authorization:$authorisation"`
+upload_output=`curl -F  'file=@app/build/outputs/apk/debug/app-debug.apk' http://localhost:8000/api/v1/upload -H "Authorization:3fc39aaa54925c9b4a311322d731023295e0e1618f0668695049b07924ca81ef"`
 printf "\nretreive hash\n"
 hash=`sed -n 's/^.*"hash": "\(.*\)",.*$/\1/p' <<< $upload_output`
 cat  $hash
 printf "\n2- scan *************\n"
-curl -X POST --url http://localhost:8000/api/v1/scan --data "scan_type=apk&file_name=app-debug.apk&hash=$hash" -H "Authorization:$authorisation"
+curl -X POST --url http://localhost:8000/api/v1/scan --data "scan_type=apk&file_name=app-debug.apk&hash=$hash" -H "Authorization:3fc39aaa54925c9b4a311322d731023295e0e1618f0668695049b07924ca81ef"
 printf "\n3- download pdf *****\n"
-curl -X POST --url http://localhost:8000/api/v1/download_pdf --data "hash=$hash&scan_type=apk" -H "Authorization:$authorisation" -o report.pdf
+curl -X POST --url http://localhost:8000/api/v1/download_pdf --data "hash=$hash&scan_type=apk" -H "Authorization:3fc39aaa54925c9b4a311322d731023295e0e1618f0668695049b07924ca81ef" -o report.pdf
 printf "\nhttp://localhost:8000/StaticAnalyzer/?name=app-debug.apk&type=apk&checksum=$hash\n"
 printf "\n4- generate json *****\n"
-curl -X POST --url http://localhost:8000/api/v1/report_json --data "hash=$hash&scan_type=apk" -H "Authorization:$authorisation" -o report.json
+curl -X POST --url http://localhost:8000/api/v1/report_json --data "hash=$hash&scan_type=apk" -H "Authorization:3fc39aaa54925c9b4a311322d731023295e0e1618f0668695049b07924ca81ef" -o report.json
 
 #printf "\n5- run glue on json file*****\n"
 #glue/lib/glue/mappings/schema.json
